85090989de5c039752da62fa03cd369fe4c0b376
tylercschneider <tylercschneider@gmail.com>
2018-05-29 14:52:51 -0400

    added routing to module

--- module.js
+++ module.js
@@ -2,4 +2,17 @@
 {
     angular
         .module("app", ["ngRoute"])
+        .config(($routeProvider) => {
+			$routeProvider
+			.when("/home", {
+				template: ""
+			})
+			.when("/movieList", {
+				template: "<movie-list></movielist>"
+			})
+			.when("/watchList", {
+				template: "<watch-list></watch-list>"
+			})
+			.otherwise({ redirectTo: "/home" });
+	});
 }
