badf2f61bdc6cd074b74f264b42666116aa1956f
tylercschneider <tylercschneider@gmail.com>
2018-05-30 14:54:44 -0400

    partial

--- service.js
+++ service.js
@@ -1,18 +1,38 @@
 
 {
     let service = function($http) {
+      let APIKey = "c4b00959d2a2303480e492eec0ddb371";
 
-    $http({
-  method: 'GET',
-  url: '/someUrl'
-}).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
-}
+      let data = {
+        title: "Our group",
+        genre: "Action",
+        length: "2 hours"
+      };
+
+      let details = {};
+
+      const getData = function() {
+        return data;
+      };
+
+      const setData = function(newData){
+        data = newData;
+      };
+      const searchMovies = function() {
+        console.log('it made it here');
+        let url = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+
+      return $http.get(url).then(function(response) {
+        console.log(response);
+        // this callback will be called asynchronously
+        // when the response is available
+         
+      })
+      }
+    };
+
+service.$inject = [$http];
+
     angular
 		.module("app")
 		.service("service", service);
--- watchlist.js
+++ watchlist.js
@@ -5,10 +5,15 @@
 	let watchList = {
 		templateUrl: "watchlist.html",
 		controller: function(service) {
+			let vm = this;
+			vm.donkey = function() {
+				console.log("button works");
+				// service.searchMovies();
+			}
 
 		}
-	}
-	watchList.$inject = ['service'];
+	};
+	watchList.$inject = ["service"];
     angular
 	.module("app")
 	.component("watchList", watchList);
