eaf42e6c9a91dfbf7c1adade0b71e2168f1b930e
tylercschneider <tylercschneider@gmail.com>
2018-06-01 14:55:01 -0400

    css coommit

--- Movies/js/service.js
+++ Movies/js/service.js
@@ -20,7 +20,7 @@
         date: 2018,
         length: "2 hours",
         overview: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
-        img: `https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg`,
+        img: `https://image.tmdb.org/t/p/w300/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg`,
         saved: false
       }];
 
